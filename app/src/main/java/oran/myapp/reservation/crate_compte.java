16784230a17feee1a5d9856d66bceabb41c90aad
hp <imenefaidi02@gmail.com>
2022-04-05 00:42:34 +0100

    erreur

---
+++ app/src/main/java/oran/myapp/reservation/crate_compte.java
@@ -0,0 +1,14 @@
+package oran.myapp.reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class crate_compte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_crate_compte );
+    }
+}
