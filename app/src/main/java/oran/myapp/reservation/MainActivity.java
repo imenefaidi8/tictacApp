7c1f695935fb045399f4a2723304f0ddbd1da790
TidelxD <luffygear37@gmail.com>
2022-04-06 00:38:20 +0000

    fix Registration bugs and add progress dialog

--- app/src/main/java/oran/myapp/reservation/MainActivity.java
+++ app/src/main/java/oran/myapp/reservation/MainActivity.java
@@ -3,6 +3,7 @@ package oran.myapp.reservation;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,13 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private TextView AddAccount;
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance ( );
-
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
-
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Sign in  ...");
+        progressDialog.setCancelable(false );
         mailEdit = findViewById ( R.id.EmailEdit );
         PassEdit = findViewById ( R.id.passwordEdit );
         LoginButton = findViewById ( R.id.loginButton );
@@ -56,14 +59,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void loginFromFirebase() {
+        progressDialog.show();
         String Tmail = mailEdit.getText ( ).toString ( );
         String TPass = PassEdit.getText ( ).toString ( );
 
         if(Tmail.isEmpty () ){
+            progressDialog.dismiss();
             mailEdit.setError ( "required ! " );
             return ;
         }
         if(TPass.isEmpty ()){
+            progressDialog.dismiss();
             PassEdit.setError ( "required !" );
             return;
 
@@ -72,11 +78,13 @@ public class MainActivity extends AppCompatActivity {
         mAuth.signInWithEmailAndPassword ( Tmail,TPass ).addOnSuccessListener ( new OnSuccessListener<AuthResult> ( ) {
             @Override
             public void onSuccess(AuthResult authResult) {
+                progressDialog.dismiss();
                 Toast.makeText ( MainActivity.this," Sign in  successfully !",Toast.LENGTH_SHORT ).show ();
             }
         } ).addOnFailureListener ( new OnFailureListener ( ) {
             @Override
             public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
                 Toast.makeText ( MainActivity.this,"error: "+e.getMessage (),Toast.LENGTH_SHORT ).show ();
             }
         } );
--- app/src/main/java/oran/myapp/reservation/RegisterActivity.java
+++ app/src/main/java/oran/myapp/reservation/RegisterActivity.java
@@ -1,24 +1,34 @@
 package oran.myapp.reservation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText txt_fullname,txt_username,txt_email,txt_password;
     private Button btn_Registre;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance ( );
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registring ...");
+        progressDialog.setCancelable(false );
         //casting views
         txt_fullname = (EditText) findViewById ( R.id.fullname );
         txt_username = (EditText) findViewById ( R.id.username );
@@ -29,11 +39,50 @@ public class RegisterActivity extends AppCompatActivity {
         btn_Registre.setOnClickListener ( new View.OnClickListener ( ) {
             @Override
             public void onClick(View view) {
-
-
+                // Method raha l ta7t
+                SignInFromFirebase ( );
 
             }
         } );
+
+    }
+    // method bach ndiru create account
+    private void SignInFromFirebase() {
+        // hada progress dialog bach ( ki ydir click tetla3 3nadah beli rah ydir loading ... )
+        progressDialog.show();
+        // njibu text mn editText w ndiruhum f variables
+        String Tmail = txt_email.getText ( ).toString ( );
+        String TPass = txt_password.getText ( ).toString ( );
+
+         // nverifiw ila edit text rahum 3amrin wela khawyin ( ila khawyin ya3tina error )
+        if(Tmail.isEmpty () ){
+            // hadi dissmiss ma3netha loading kamel ( ya error ya succeed wa7da fihum )
+            progressDialog.dismiss();
+            txt_email.setError ( "required ! " );
+            return ;
+        }
+        if(TPass.isEmpty ()){
+            progressDialog.dismiss();
+            txt_password.setError ( "required !" );
+            return;
+
+        }
+        // Hna ndiru create user with email and password ( create account bel email wel mot de pass )
+
+        mAuth.createUserWithEmailAndPassword ( Tmail,TPass ).addOnSuccessListener ( new OnSuccessListener<AuthResult>( ) {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                progressDialog.dismiss();
+                Toast.makeText ( RegisterActivity.this," Sign up  successfully !",Toast.LENGTH_SHORT ).show ();
+            }
+        } ).addOnFailureListener ( new OnFailureListener( ) {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText ( RegisterActivity.this,"error: "+e.getMessage (),Toast.LENGTH_SHORT ).show ();
+            }
+        } );
+
 
     }
 }
